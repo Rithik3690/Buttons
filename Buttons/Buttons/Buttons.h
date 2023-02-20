3a9e41856f89dbd1d4a9a06dcf1581ab64dd64ba
Rithik <rithik.vardhan@12thwonder.com>
2023-02-20 13:43:21 +0530

    Creating a Framework

---
+++ Buttons/Buttons/Buttons.h
@@ -0,0 +1,18 @@
+//
+//  Buttons.h
+//  Buttons
+//
+//  Created by Rithik Vardhan on 20/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Buttons.
+FOUNDATION_EXPORT double ButtonsVersionNumber;
+
+//! Project version string for Buttons.
+FOUNDATION_EXPORT const unsigned char ButtonsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Buttons/PublicHeader.h>
+
+
